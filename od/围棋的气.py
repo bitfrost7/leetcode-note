ec57d3c330bcd493a6bd0d645d6be304b299dca8
邹海强 <15591017398@163.com>
2024-03-23 01:20:15 +0800

    1

--- od/围棋的气.py
+++ od/围棋的气.py
@@ -11,3 +11,52 @@
 # 3、本题目只计算气，对于眼也按气计算，如果您不清楚：眼"的概念，可忽略，按照前面描述的规则计算
 # 即可。
 # 现在，请根据输入的黑棋和白棋的坐标位置，计算黑棋和白起一共各有多少气？
+# 假设输入：
+# 0 5 8 9 9 10
+# 5 0 9 9 9 8
+# 输出：
+# 8 7
+
+def neighbor(x, y, board_size):
+    neighbors = []
+    if x + 1 < board_size:
+        neighbors.append((x + 1, y))
+    if x - 1 >= 0:
+        neighbors.append((x - 1, y))
+    if y + 1 < board_size:
+        neighbors.append((x, y + 1))
+    if y - 1 >= 0:
+        neighbors.append((x, y - 1))
+
+    return neighbors
+
+
+def calc_liberty(black_co_list, white_co_list):
+    board_size = 19
+    black_cnt, white_cnt = 0, 0
+    for i in range(0, board_size - 1):
+        for j in range(0, board_size - 1):
+            co = (i, j)
+            if co not in black_co_list and co not in white_co_list:
+                has_black, has_white = False, False
+                for nei_co in neighbor(i, j, 19):
+                    if not has_black and nei_co in black_co_list:
+                        black_cnt += 1
+                        has_black = True
+                    if not has_white and nei_co in white_co_list:
+                        white_cnt += 1
+                        has_white = True
+    return black_cnt, white_cnt
+
+
+def main():
+    line1 = list(map(int, input().split()))
+    black_co_list = [tuple(line1[i:i + 2]) for i in range(0, len(line1), 2)]
+    line2 = list(map(int, input().split()))
+    white_co_list = [tuple(line2[i:i + 2]) for i in range(0, len(line2), 2)]
+    black_cnt, white_cnt = calc_liberty(black_co_list, white_co_list)
+    print(black_cnt, white_cnt)
+
+
+if __name__ == '__main__':
+    main()
